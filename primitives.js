363837ace8aa9b489954e79f2d39822dccee3143
GR0110 <gangadharreddym0101@gmail.com>
2025-01-22 15:23:34 -0500

    changes are done

---
+++ primitives.js
@@ -0,0 +1,39 @@
+/*
+TYPES OF DATA 1) PRIMITIVE & Non PRIMITIVE 
+PEIMITIVE --------- NUMBERS, BOOLEAN, NULL, UNDEFINED, STRING, SYMBOL.
+Non PRIMITIVE------ ARRAY, FUNCTIONS, OBJECT
+*/
+
+
+//Numbers
+
+let balance = 0110;
+console.log(typeof balance)
+
+//boolean
+
+let isActive = true;
+console.log(typeof isActive)
+
+//null and undefined 
+
+ let firstname;
+ let lastname = null;
+ console.log(firstname)
+ console.log(lastname)
+
+ //String
+
+ let fName = "Reddy";
+ let lName = 'Muskula';
+
+ let greetmessage = `Hello ${fName} ${lName}`;
+
+ console.log(typeof fName);
+ console.log(typeof lName);
+ console.log(typeof greetmessage);
+
+ console.log( fName);
+ console.log( lName);
+ console.log( greetmessage);
+ console.log( "Hi", fName, lName);
