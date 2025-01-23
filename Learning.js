f16dcb282a934da54dddd0605f702a6e05553952
bharathk19 <43506154+bharathk19@users.noreply.github.com>
2025-01-23 13:19:22 -0500

    Create Learning.js

---
+++ Learning.js
@@ -0,0 +1,11 @@
+// Write a code to check if a string is palindrome eg: ABCDCBA 
+
+// Write a code to print the length of a string and print the string in reverse
+
+// compare 2 strings are equal with out using predefined methods such as Contains, includes.
+
+// print all palindrome numbers from ranging 10-2000
+
+// Sort a Array with any numbers you like.
+
+// find lanrgest number in a unsorted array.
